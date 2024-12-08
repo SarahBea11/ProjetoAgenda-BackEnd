efe45f53b0c80fbad6147df0ee1358600d342df0
SarahBea11 <sarahbealimapina@gmail.com>
2024-12-08 12:34:56 -0300

    Configurando os Serviços

--- src/main/java/com/projetoagenda/agenda/services/AgendaService.java
+++ src/main/java/com/projetoagenda/agenda/services/AgendaService.java
@@ -1,8 +1,24 @@
 package com.projetoagenda.agenda.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.projetoagenda.agenda.dtos.AgendaResponse;
+import com.projetoagenda.agenda.mappers.AgendaMappers;
+import com.projetoagenda.agenda.repositories.AgendaRepository;
 
 @Service
 public class AgendaService {
     
+    @Autowired
+    private AgendaRepository repository;
+
+    public List<AgendaResponse> getAllAgenda(){
+        return repository.findAll().stream()
+                                   .map(p -> AgendaMappers.toDTO(p))
+                                   .collect(Collectors.toList());
+    }
 }

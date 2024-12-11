8c8d028b504836f5d48d79c78f5ed5fb5a8d6922
SarahBea11 <sarahbealimapina@gmsil.com>
2024-12-10 22:49:39 -0300

    back-end junto do front com validação

--- src/main/java/com/projetoagenda/agenda/controllers/AgendaController.java
+++ src/main/java/com/projetoagenda/agenda/controllers/AgendaController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoagenda.agenda.dtos.AgendaRequest;
 import com.projetoagenda.agenda.dtos.AgendaResponse;
 import com.projetoagenda.agenda.services.AgendaService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("agenda")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AgendaController {
     
     @Autowired 
@@ -42,13 +45,13 @@ public class AgendaController {
     }
     
     @PostMapping()
-    public ResponseEntity<AgendaResponse> saveAgenda(@RequestBody AgendaRequest agenda){
+    public ResponseEntity<AgendaResponse> saveAgenda(@Validated @RequestBody AgendaRequest agenda){
         AgendaResponse newAgenda = service.save(agenda);
         return ResponseEntity.created(null).body(newAgenda);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Void> updateAgenda(@PathVariable long id, @RequestBody AgendaRequest agenda){
+    public ResponseEntity<Void> updateAgenda(@Validated @PathVariable long id, @RequestBody AgendaRequest agenda){
         service.update(agenda, id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/projetoagenda/agenda/dtos/AgendaRequest.java
+++ src/main/java/com/projetoagenda/agenda/dtos/AgendaRequest.java
@@ -1,8 +1,14 @@
 package com.projetoagenda.agenda.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record AgendaRequest(
+    @NotNull(message = "namecommitment não pode ser nulo")
     String namecommitment,
+    @NotBlank(message = "localcommitment não pode ser nulo")
     String localcommitment,
+    @NotBlank(message = "Date não pode ser nulo")
     String date
 ) {
     
